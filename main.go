6f35344227588b7080354239b25c58be3d922ca7
dirkarnez <smalldirkalex@gmail.com>
2019-11-07 03:18:25 +0800

    - add triggered task sample

--- main.go
+++ main.go
@@ -1,23 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/dirkarnez/wait2die"
 	"github.com/play175/wifiNotifier"
 	"log"
+	"os/exec"
 )
 
-func main() {
-	var previousSSID = ""
+var previousSSID = ""
 
+func main() {
+
+	wifiNotify(func(ssid string) {
+		if ssid == "Default"{
+			Chrome()
+		}
+	})
+
+	wait2die.WaitToDie(nil)
+}
+
+type subscriber func(ssid string)
+
+func wifiNotify(subscribers... subscriber) {
+	var dispatcher = func(ssid string) {
+		for _, s := range subscribers {
+			s(ssid)
+		}
+	}
 	wifiNotifier.SetWifiNotifier(func(ssid string) {
 		if  previousSSID == ssid {
 			return
 		}
+
+		dispatcher(ssid)
+
 		log.Println("onWifiChanged, current ssid:" + ssid)
 		previousSSID = ssid
 	})
 
-	log.Println("current ssid:" + wifiNotifier.GetCurrentSSID())
-
-	wait2die.WaitToDie(nil)
+	dispatcher(wifiNotifier.GetCurrentSSID())
+}
+func Chrome() {
+	cmd := exec.Command("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe","--chrome-frame", "--app=https://google.com")
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
 }
